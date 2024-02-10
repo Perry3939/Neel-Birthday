e371fd6fb7037aabedbb7c93f363084bb4987eb7
Perry <perry@pop-os.localdomain>
2024-02-10 17:39:58 +0530

    Adding Birthday Files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Intro from "./pages/introPage";
+import DateNight from "./pages/dateNight";
+import Date from "./pages/date";
+import Final from "./pages/final";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/datenight" element={<DateNight />} />
+        <Route path="/date" element={<Date />} />
+        <Route path="/final" element={<Final />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
